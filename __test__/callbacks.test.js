1c0149e3dde514b81ba22570dddadd1d48aee3e5
Cristian Bueno <cbueno.cb@gmail.com>
2021-04-29 22:40:09 +0200

    Instalar axios y testear setup, promesas, callbacks y async

---
+++ __test__/callbacks.test.js
@@ -0,0 +1,17 @@
+import { callbackHell } from '../callbacks';
+
+
+describe('Probando un Callback', () => {
+
+    test('Callback', done => {
+
+        function otherCallback(data) {
+            expect(data).toBe('Hola JavaScripters');
+            done();
+        }
+
+        callbackHell(otherCallback);
+        
+    });
+
+});
